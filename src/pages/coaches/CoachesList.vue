3a6ad70980d4be9d12db80e7a03581f19cd20345
rmnvnc <81642663+rmnvnc@users.noreply.github.com>
2023-12-07 20:31:12 +0100

    coach component

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -7,25 +7,49 @@
             <button>Refresh</button>
             <Router-link to="/register">Register as a coach</router-link>
         </div>
-        <ul>
-            <li v-for="coach in filteredCoaches" :key="coach.id">
-                {{ coach.firstName }}
-            </li>
+        <ul v-if="hasCoaches">
+            <coach-item 
+                v-for="coach in filteredCoaches" 
+                :key="coach.id" 
+                :id="coach.id" 
+                :first-name="coach.firstName"
+                :last-name="coach.lastName"
+                :rate="coach.hourlyRate"
+                :areas="coach.areas">
+            </coach-item>
         </ul>
+        <h3 v-else>No coaches found :/</h3>
     </section>
 </template>
 
 <script>
+import CoachItem from '../../components/coaches/CoachItem.vue';
+
 export default {
+    components: {
+        CoachItem
+    },
     computed: {
         filteredCoaches() {
             return this.$store.getters['coaches/coaches']
+        },
+        hasCoaches() {
+            return this.$store.getters['coaches/hasCoaches']
         }
     }
 }
 
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
 
+.controls {
+  display: flex;
+  justify-content: space-between;
+}
 </style>
