d04440f05e54689a8d69aa0df9bbec34ab4e8bbd
rmnvnc <81642663+rmnvnc@users.noreply.github.com>
2025-05-02 22:46:16 +0200

    user - presunutie config do local env

--- src/config.js
+++ src/config.js
@@ -1,5 +1,7 @@
-const firebase = '';
+const firebase = process.env.VUE_APP_FIREBASE_DB;
+const firebaseAPIkey = process.env.VUE_APP_FIREBASE_API_KEY;
 
 export default {
-    firebase: firebase
+    firebase: firebase,
+    firebaseAPIkey: firebaseAPIkey
 }
