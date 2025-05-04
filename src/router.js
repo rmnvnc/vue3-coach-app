b1e81d8e5c90506f617965a9bc96ef45ba1a4eca
rmnvnc <81642663+rmnvnc@users.noreply.github.com>
2025-05-04 14:26:51 +0200

    optimatization - async components, async routes

--- src/router.js
+++ src/router.js
@@ -1,14 +1,20 @@
 import { createRouter, createWebHistory } from "vue-router";
 
 
-import CoachDetail from './pages/coaches/CoachDetail.vue';
+// import CoachDetail from './pages/coaches/CoachDetail.vue';
 import CoachesList from './pages/coaches/CoachesList.vue';
-import CoachRegistration from './pages/coaches/CoachRegistration.vue';
-import ContactCoach from './pages/requests/ContactCoach.vue';
-import RequestReceived from './pages/requests/RequestReceived.vue';
+// import CoachRegistration from './pages/coaches/CoachRegistration.vue';
+// import ContactCoach from './pages/requests/ContactCoach.vue';
+// import RequestReceived from './pages/requests/RequestReceived.vue';
 import NotFound from './pages/NotFound.vue';
-import UserAuth from "./pages/auth/UserAuth.vue";
+// import UserAuth from './pages/auth/UserAuth.vue';
 import store from './store/index.js';
+
+const CoachDetail = () => import('./pages/coaches/CoachDetail.vue');
+const CoachRegistration = () => import('./pages/coaches/CoachRegistration.vue');
+const ContactCoach = () => import('./pages/requests/ContactCoach.vue');
+const RequestReceived = () => import('./pages/requests/RequestReceived.vue');
+const UserAuth = () => import('./pages/auth/UserAuth.vue');
 
 const router = createRouter({
     history: createWebHistory(),
