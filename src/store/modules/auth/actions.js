838717bd73e5f7d435373a0d83412299bbca4fc1
rmnvnc <81642663+rmnvnc@users.noreply.github.com>
2025-05-04 13:11:40 +0200

    user - refactor login/logout

--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -1,6 +1,24 @@
 export default {
     async login(context, payload) {
-        const response = await fetch(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${context.rootGetters.config.firebaseAPIkey}`, {
+        return context.dispatch('auth',{
+            ...payload,
+            mode: 'login'
+        })
+    },
+    async signup(context, payload) {
+        return context.dispatch('auth',{
+            ...payload,
+            mode: 'signup'
+        })
+    },
+    async auth(context, payload) {
+        const mode = payload.mode;
+        let url = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${context.rootGetters.config.firebaseAPIkey}`
+
+        if (mode === 'signup') {
+            url = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${context.rootGetters.config.firebaseAPIkey}`
+        }
+        const response = await fetch(url, {
             method: 'POST',
             body: JSON.stringify({
                 email: payload.email,
@@ -12,42 +30,13 @@ export default {
         const responseData = await response.json();
 
         if (!response.ok) {
-            console.log(responseData);
             const error = new Error(responseData.message || 'Failet to authenticate. Check your login data.')
             throw error;
         }
-
-        console.log(responseData);
 
         context.commit('setUser', {
             token: responseData.idToken,
             userId: responseData.localId,
-            tokenExpiration: responseData.expiresIn
-        })
-    },
-    async signup(context, payload) {
-        const response = await fetch(`https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${context.rootGetters.config.firebaseAPIkey}`, {
-            method: 'POST',
-            body: JSON.stringify({
-                email: payload.email,
-                password: payload.password,
-                returnSecureToken: true
-            })
-        });
-
-        const responseData = await response.json();
-
-        if (!response.ok) {
-            console.log(responseData);
-            const error = new Error(responseData.message || 'Failet to authenticate. Check your login data.')
-            throw error;
-        }
-
-        console.log(responseData);
-
-        context.commit('setUser', {
-            token: responseData.idToken,
-            usetId: responseData.localId,
             tokenExpiration: responseData.expiresIn
         })
     },
